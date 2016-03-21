1b0d34661787a0cf313ade668c6578a11602cc95
chengyitong <cyt@TaidymatoMacBook-Air.local>
2016-03-21 16:04:01 +0800

    update

--- AmerAdmin/js/common.js
+++ AmerAdmin/js/common.js
@@ -1,8 +1,8 @@
 // 菜单栏切换
-function tab (data_id){
+function tab(data_id) {
 	var $item = $('.menu-item a'),
-	$main = $('.main');
-	for (var i = 0,len = $item.length;i < len;i++){
+		$main = $('.main');
+	for (var i = 0, len = $item.length; i < len; i++) {
 		$item.eq(i).parent().removeClass('active');
 		$main.eq(i).addClass('fn-hide');
 
@@ -15,25 +15,56 @@ function tab (data_id){
 	}
 }
 
-window.onload = function(){
+// 显示警告框
+function showMsg(msg, type) {
+	var type_class = '';
+	if (type == '') { //默认
+		type_class = '';
+	} else if (type == 'w') {
+		type_class = 'page-msg-warning';
+	} else if (type == 's') {
+		type_class = 'page-msg-success';
+	}
+
+	var msg_box = '<div class="page-msg ' + type_class + '" id="page_msg"><a href="#" class="close">&times;</a><em>' + msg + '</em></div>';
+	$('body').append(msg_box);
+
+	// 关闭警告框
+	$('.close').click(function() {
+		$('.page-msg').remove();
+	});
+}
+
+window.onload = function() {
+	// 菜单栏显示
 	var hash = window.location.hash,
-	hash_len = hash.length,
-	hash_r = hash.substring(1,hash_len);
+		hash_len = hash.length,
+		hash_r = hash.substring(1, hash_len);
 
 	if (hash_r == '') {
 		hash_r = 'home';
 	}
 	tab(hash_r);
 
-	$('.menu-item a').click(function(){
+	$('.menu-item a').click(function() {
 		var data_id = $(this).attr('data-id');
-
 		tab(data_id);
 	});
 
-	// 帮助提示
-	$('.help').mouseover(function() {
-		$('.help_content').toggle();
+	// 问号帮助提示
+	var timer = '';
+	$('.help-icon').mouseover(function() {
+		window.clearInterval(timer);
+		$(this).siblings('.help_content').show();
+	});
+	$('.help-icon').mouseout(function() {
+		timer = setInterval("$('.help-icon').siblings('.help_content').hide();", 1000);
+	});
+	$('.help_content').mouseover(function() {
+		window.clearInterval(timer);
+	});
+	$('.help_content').mouseout(function() {
+		timer = setInterval("$('.help_content').hide();", 1000);
 	});
 
 	// 分页功能
@@ -44,13 +75,13 @@ window.onload = function(){
 	kkpager.total = totalPage;
 	kkpager.totalRecords = totalRecords;
 	kkpager.generPageHtml({
-		pno : pageindex,
+		pno: pageindex,
 		//总页码
-		total : totalPage,
+		total: totalPage,
 		//总数据条数
-		totalRecords : totalRecords,
-		mode : 'click', //默认值是link，可选link或者click
-		click : function (n) {
+		totalRecords: totalRecords,
+		mode: 'click', //默认值是link，可选link或者click
+		click: function(n) {
 			this.selectPage(n);
 			// 这里添加点击页面跳转时调用的函数
 			return false;
